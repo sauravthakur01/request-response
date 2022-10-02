5b354562b67cc640f6471b1165d0a0760d550f05
Sanjay Pal <sanjaypal001@gmail.com>
2022-10-02 17:12:13 +0530

    routes

--- first-app.js
+++ first-app.js
@@ -1,12 +1,31 @@
-const http =require('http');
+const http = require("http");
 
-const server = http.createServer((req,res)=>{
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body>Welcome to my Node Js project</body>')
-    res.write('</html>')
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url == "/about") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<body> Welcome to About Us page</body>");
+    res.write("</html>");
     res.end;
-})
+  }
+  if (url == "/home") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<body> Welcome home</body>");
+    res.write("</html>");
+    res.end;
+  }
+  if (url == "/node") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>My First Page</title></head>");
+    res.write("<body>Welcome to my Node Js project</body>");
+    res.write("</html>");
+    res.end;
+  }
+});
 
 server.listen(4000);
