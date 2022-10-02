593ed2d7e48c331a0ae4e36e6e21e46eace747f8
Sanjay Pal <sanjaypal001@gmail.com>
2022-10-02 19:53:52 +0530

    handling redirect and stream and buffer

--- first-app.js
+++ first-app.js
@@ -1,31 +1,37 @@
+
 const http = require("http");
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  const url = req.url;
-  if (url == "/about") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>My First Page</title></head>");
-    res.write("<body> Welcome to About Us page</body>");
-    res.write("</html>");
-    res.end;
-  }
-  if (url == "/home") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>My First Page</title></head>");
-    res.write("<body> Welcome home</body>");
-    res.write("</html>");
-    res.end;
-  }
-  if (url == "/node") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>My First Page</title></head>");
-    res.write("<body>Welcome to my Node Js project</body>");
-    res.write("</html>");
-    res.end;
-  }
+    const method = req.method;
+    const url = req.url;
+    if (url === '/') {
+        res.setHeader("Content-Type", "text/html");
+        res.write('<html>');
+        res.write('<head><title>My First Page</title></head>');
+        res.write('<body><form action="/message" method="POST" ><input type="text" name="message" ><button>Submit</button></form></body>');
+        res.write('</html>');
+        res.end;
+    }
+    if (url === "/message" && method=="POST") {
+
+        const body =[];
+
+        req.on('data',(chunk)=>{
+            body.push(chunk);
+        })
+        return req.on('end' , ()=>{
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message = parsedBody.split('=')[1];
+            fs.writeFile('message.txt' , message , err=> {
+                res.statusCode= 302 ;
+                res.setHeader('Location' , '/')
+                res.end();
+            });
+        })
+    }
+
 });
 
 server.listen(4000);
