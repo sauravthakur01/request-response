802435fb9f70b2e6d7452cf57dbf812736df6e5b
Sanjay Pal <sanjaypal001@gmail.com>
2022-10-04 18:10:39 +0530

    filter and adding page not found

--- first-app.js
+++ first-app.js
@@ -1,20 +1,18 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
+
+const app = express();
+
+const adminRouter = require('./routes/admin');
+const shopRouter =require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/add-product',(req,res,next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Submit product</button></input></form>')
-})
+app.use('/admin', adminRouter);
+app.use('/shop', shopRouter);
 
-app.post("/product" , (req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-})
-
-app.use('/',(req,res,next)=>{
-    res.send('<h1>Welcome to homepage</h1>')
+app.use((req,res,next)=>{
+    res.status(404).send('<h1>Page not found</h1>');
 })
 
 app.listen(3000);
---
+++ routes/admin.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/add-product',(req,res,next)=>{
+    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title"><input type="number" name="size"><button type="submit">Submit product</button></input></form>')
+})
+
+router.post("/add-product" , (req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/shop/');
+})
+
+module.exports = router ;
